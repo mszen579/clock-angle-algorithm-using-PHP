b99f40fb851d0387ab2f879cef1d442a75ad01be
M ALZEN <35220325+mszen579@users.noreply.github.com>
2018-04-01 10:25:54 +0200

    Update Clock.class.php

--- Clock.class.php
+++ Clock.class.php
@@ -21,3 +21,37 @@ function AngleOfClock($hours, $minutes) {
 }
 
 AngleOfClock(12, 15);
+
+
+  /* PSEUDO CODE:
+To get the Angle on the clock:
+1 - Create a function called AngleOfClock
+2 - Divid the hours by 12
+3 - Create variables for minutes and hours
+4 - Allocate math formulas to define hours and the minutes parts
+5 - Apply a built in function 'abs'  to calculate the total angle corresponding to the chosen hour
+6 - Create a condition to check and echo only the angle between the clock hands
+7 - Call the function AngleOfClock
+
+
+
+    T-DIAGRAM:
+---------------
+We need to get the AngleOfClock of (10, 30)
+
+Var	                                      |values
++-----------------------------------------+-----------------------
+$hours                                    |10 
+$minutes                                  |30
+the angel is less than 180                |135 degrees
+
+//--------------------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+
+
