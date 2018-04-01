ee5fd695f2a5336d7e5a84952b9779aa8c6758df
M ALZEN <35220325+mszen579@users.noreply.github.com>
2018-04-01 10:23:49 +0200

    Update Clock.class.php

--- Clock.class.php
+++ Clock.class.php
@@ -4,42 +4,22 @@
 #
 # Clock class
 
-class Clock {
-    
-    protected static $hour;
-    protected static $minute;
-    
-    public function __construct() {
-        self::$hour = date("g");
-        self::$minute = date("i");
-    }
-    
-    // Get the angle based on the current hour / minute
-    public function currentAngle() {
-        $h = self::$hour;
-        $m = self::$minute;
+function AngleOfClock($hours, $minutes) { 
+  $hours = $hours % 12;
+  
+      $hourMinPart = 0.5 * $minutes; 
+      $hourHourPart = 30 * $hours; 
+      $minAngle = 6 * $minutes; 
+      $totalAngle = abs($hourMinPart + $hourHourPart - $minAngle); 
+      
+        if ($totalAngle<=180){
+        echo $totalAngle .' ' .'degrees'; 
+        }
         
-        return $this->angle($h, $m);
-    }
-
-    // Core angle logic
-    protected function angle($h, $m) {
-        $angle = abs((0.5 * ((($h % 12) * 60) + $m)) - 6 * $m);
-        return ($angle > 180) ? (180 - $angle % 180) : $angle;
-    }
-    
-    // I prefer the factory method, "Class::factory();" vs just "new Class;"
-    public static function factory() {
-        return new self;
-    }
-    
-    // Build a user-friendly time string
-    public function getTimeString() {
-        $h = self::$hour;
-        $m = self::$minute;
-        
-        $time_strong = $h . ":" . $m;
-        return $time_strong;
-    }
-    
+        else{
+        echo (360 - $totalAngle) .' ' .'degrees'; 
+        } 
+  
 }
+
+AngleOfClock(12, 15);
